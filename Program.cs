ff7ded3aba99e705e41183ca3e29e27aba95e9bb
KiYari <termrdz@gmail.com>
2023-10-02 15:36:52 +0300

    completed tasks 04

--- Program.cs
+++ Program.cs
@@ -1,33 +1,34 @@
-﻿Console.WriteLine(GetSecontDigit(658));
-Console.WriteLine(GetSecontDigit(704) + "\n");
+﻿Console.WriteLine(Pow(3, 5));
+Console.WriteLine(Pow(2, 4) + '\n');
 
-Console.WriteLine(GetThirdDigit(77118829));
-Console.WriteLine(GetThirdDigit(2) + "\n");
+Console.WriteLine(GetDigitSum(82 ));
+Console.WriteLine(GetDigitSum(452 ));
+Console.WriteLine(GetDigitSum(9012 ) + "\n");
 
-Console.WriteLine(IsItAWeekday(4));
-Console.WriteLine(IsItAWeekday(28));
-Console.WriteLine(IsItAWeekday(15));
-Console.WriteLine(IsItAWeekday(6));
-Console.WriteLine(IsItAWeekday(7));
+GetArray(1, 2, 5, 7, 19);
+GetArray(6, 1, 33);
 
 
-char GetSecontDigit(int number) {
-    return number.ToString()[1];
-}
-
-char GetThirdDigit(int number) {
-    try {
-        return number.ToString()[2];
-    } catch (Exception e) {
-        Console.WriteLine("третьей цифры нет");
-    }
-    return '\0';
-}
-
-string IsItAWeekday(int num) {
-    if ((num % 6 == 0) || (num % 7 == 0)) {
-        return "да";
+int Pow(int number, int power) {
+    int res = number;
+    for(int i = 0; i < power; i++) {
+        res *= number;
     }
 
-    return "нет";
+    return res;
+}
+
+int GetDigitSum(int num) {
+    char[] digitArray = num.ToString().ToCharArray();
+    int res = 0;
+
+    for(int i = 0; i < digitArray.Length; i++) {
+        res += digitArray[i] - '0';
+    }
+
+    return res;
+}
+
+void GetArray(params int[] arrayInitValues) {
+    Console.WriteLine("[" + string.Join(", ", arrayInitValues) + "]");
 }
